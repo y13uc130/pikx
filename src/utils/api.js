a243b1e9e3dce9b81d64a80be380d3b0ec904478
Intel <intel@Intel-Mac.local>
2019-10-01 19:44:40 +0530

    tunnel change

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://5c41235f.ngrok.io/api/v1';
+axios.defaults.baseURL = 'http://5e145f8a.ngrok.io/api/v1';
 
 axios.defaults.headers.common['Pragma'] = 'no-cache';
 axios.defaults.headers.common['Cache-Control'] = 'no-store, must-revalidate';
